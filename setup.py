86e35b449dcfadc188cf9ff84297e6f1940352e8
ArneNx <arnenix@gmail.com>
2019-11-25 11:22:19 +0100

    Added docker stuff to run on Sinzlab server

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='transfer_learning',
+   version='0.1dev',
+   description='Experiments about inductive bias transfer',
+   author='Arne Nix',
+   author_email='arnenix@googlemail.com',
+   packages=['transfer_learning'],  #same as name
+   install_requires=[], #external packages as dependencies
+)
