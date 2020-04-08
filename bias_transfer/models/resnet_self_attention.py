f3a24be54a4778f2817d2351b727f403fae779a6
ArneNx <arnenix@gmail.com>
2020-04-08 11:11:01 +0200

    Renamed readout in all architectures to 'readout'

--- bias_transfer/models/resnet_self_attention.py
+++ bias_transfer/models/resnet_self_attention.py
@@ -89,7 +89,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.dense = nn.Linear(512 * block.expansion, num_classes)
+        self.readout = nn.Linear(512 * block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1] * (num_blocks - 1)
@@ -107,7 +107,7 @@ class ResNet(nn.Module):
         out = self.layer4(out)
         out = F.avg_pool2d(out, 4)
         core_out = out.view(out.size(0), -1)
-        out = self.dense(core_out)
+        out = self.readout(core_out)
 
         return {"logits": out, "conv_rep": core_out}
 
