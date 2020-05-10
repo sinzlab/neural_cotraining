Added noise transform (to apply noise before normalization) and option to deactivate clamping of noise + some fixes for analysis

bias_transfer/analysis/analyzer.py
@@ -1,4 +1,6 @@
 import math
+from functools import partial
+
 import torch
 import torch.backends.cudnn as cudnn
 from bias_transfer.utils.io import load_checkpoint
@@ -42,6 +44,37 @@ class Analyzer:
         df = pd.concat([df.drop([1], axis=1), df[1].apply(pd.Series)], axis=1)
         df = df.rename(columns={0: "training_progress"})
         df = pd.concat([df.drop([1], axis=1), df[1].apply(pd.Series)], axis=1)
+
+        def convert(x, prefix=""):
+            ret = x["img_classification"]
+            ret = {prefix + k: v for k, v in ret.items()}
+            return ret
+
+        df = pd.concat(
+            [
+                df.drop(["test_results"], axis=1),
+                df["test_results"]
+                .apply(partial(convert, prefix="test_"))
+                .apply(pd.Series),
+            ],
+            axis=1,
+        )
+        df["dev_eval"] = df["dev_eval"].apply(lambda x: x["img_classification"])
+
+        def convert(x, key="epoch_loss"):
+            if key in x.keys():
+                return x[key]
+            if "img_classification" in x.keys():
+                return convert(x["img_classification"], key)
+            else:
+                return {k: convert(v,key) for k,v in x.items()}
+
+        df["dev_noise_eval"] = df["dev_final_results"].apply(partial(convert, key="eval"))
+        df["dev_noise_loss"] = df["dev_final_results"].apply(partial(convert, key="epoch_loss"))
+        df= df.drop(["dev_final_results"], axis=1)
+        df["c_test_eval"] = df["test_c_results"].apply(partial(convert, key="eval"))
+        df["c_test_loss"] = df["test_c_results"].apply(partial(convert, key="epoch_loss"))
+        df= df.drop(["test_c_results"], axis=1)
         return df
 
     def plot(
@@ -51,6 +84,7 @@ class Analyzer:
         save="",
         perf_measure="dev_eval",
         style="lighttalk",
+        legend_outside=True
     ):
         if not to_plot in ("c_test_eval", "c_test_loss"):
             fs = (16, 10) if "talk" in style else (12, 7.5)
@@ -68,7 +102,7 @@ class Analyzer:
                 sns.set_context("paper")
             fig, ax = plt.subplots(figsize=fs, dpi=dpi)
         # Plot
-        if to_plot in ("test_eval", "test_loss"):
+        if to_plot in ("test_eval", "test_epoch_loss"):
             sns.barplot(x="name", y=to_plot, hue="name", data=self.df, ax=ax)
         elif to_plot in ("dev_noise_eval", "dev_noise_loss"):
             data = self.df[to_plot].apply(pd.Series)
@@ -140,11 +174,14 @@ class Analyzer:
                     del data["Corruption"]
                     # print(data)
                     sns.heatmap(data, annot=True, cbar=False)
+
                 g.map_dataframe(draw_heatmap)
                 fig = g.fig
         elif to_plot in ("training_progress",):
             data = self.df[to_plot].apply(pd.Series)
-            data = data.applymap(lambda x: x.get(perf_measure) if isinstance(x,dict) else None)
+            data = data.applymap(
+                lambda x: x.get(perf_measure) if isinstance(x, dict) else None
+            )
             data = pd.concat([self.df["name"], data], axis=1)
             data.index = data.name
             del data["name"]
@@ -170,7 +207,12 @@ class Analyzer:
                         label.set_visible(False)
                     ax.xaxis.offsetText.set_visible(False)
         if "talk" in style:
-            plt.legend(fontsize=14, title_fontsize="14")
+            if legend_outside:
+                plt.legend(fontsize=14, title_fontsize="14", bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+            else:
+                plt.legend(fontsize=14, title_fontsize="14")
+        elif legend_outside:
+            plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
         if save:
             fig.savefig(
                 save + "_" + style,
@@ -223,4 +265,3 @@ def print_table_for_excel(table):
             for res in final_results[key].items():
                 print(res[1], end=",")
         print()
-

bias_transfer/analysis/representation_analysis.py
@@ -36,8 +36,8 @@ class RepresentationAnalyser:
         ).load_model(include_trainer=True, include_state_dict=True, seed=42)
         self.num_samples = -1
         self.sample_loader = torch.utils.data.DataLoader(
-            data_loaders[dataset].dataset,
-            sampler=data_loaders[dataset].sampler,
+            data_loaders[dataset]["img_classification"].dataset,
+            sampler=data_loaders[dataset]["img_classification"].sampler,
             batch_size=64,
             shuffle=False,
             num_workers=1,

bias_transfer/dataset/img_dataset_loader.py
@@ -9,6 +9,7 @@ from torchvision import datasets
 from bias_transfer.configs.dataset import ImageDatasetConfig
 from bias_transfer.dataset.utils import get_dataset, create_ImageFolder_format
 from bias_transfer.dataset.npy_dataset import NpyDataset
+from bias_transfer.trainer.main_loop_modules import NoiseAugmentation
 from .dataset_filters import *
 
 DATASET_URLS = {
@@ -55,33 +56,91 @@ def img_dataset_loader(seed, **config):
     config = ImageDatasetConfig.from_dict(config)
     torch.manual_seed(seed)
     np.random.seed(seed)
-    transform_list_base = [transforms.ToTensor()]
-    if config.apply_grayscale:
-        transform_list_base = [transforms.Grayscale()] + transform_list_base
-    if config.apply_normalization:
-        transform_list_base += [
-            transforms.Normalize(config.train_data_mean, config.train_data_std)
-        ]
-    if config.dataset_cls == "ImageNet" and config.apply_augmentation:
-        transform_list = [
-            transforms.RandomResizedCrop(config.input_size),
-            transforms.RandomHorizontalFlip(),
-        ] + transform_list_base
-    elif config.apply_augmentation:
-        transform_list = [
-            transforms.RandomCrop(config.input_size, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomRotation(15),
-        ] + transform_list_base
-    else:
-        transform_list = transform_list_base
+    if config.apply_noise:
+
+        def apply_noise(x):
+            if config.apply_noise.get("noise_std"):
+                std = config.apply_noise.get("noise_std")
+                noise_config = {
+                    "std": {np.random.choice(list(std.keys()), p=list(std.values())): 1.0}
+                }
+            elif config.apply_noise.get("noise_snr"):
+                snr = config.apply_noise.get("noise_snr")
+                noise_config = {
+                    "snr": {np.random.choice(list(snr.keys()), p=list(snr.values())): 1.0}
+                }
+            return NoiseAugmentation.apply_noise(x, device="cpu", **noise_config)[0]
+
     if config.dataset_cls == "ImageNet":
-        transform_list_base = [
+        transform_train = [
+            transforms.RandomResizedCrop(config.input_size)
+            if config.apply_augmentation
+            else None,
+            transforms.RandomHorizontalFlip() if config.apply_augmentation else None,
+            transforms.Grayscale() if config.apply_grayscale else None,
+            transforms.ToTensor(),
+            apply_noise if config.apply_noise else None,
+            transforms.Normalize(config.train_data_mean, config.train_data_std)
+            if config.apply_normalization
+            else None,
+        ]
+        transform_val = [
             transforms.Resize(256),
             transforms.CenterCrop(224),
-        ] + transform_list_base
-    transform_base = transforms.Compose(transform_list_base)
-    transform_train = transforms.Compose(transform_list)
+            transforms.Grayscale() if config.apply_grayscale else None,
+            transforms.ToTensor(),
+            transforms.Lambda(apply_noise) if config.apply_noise else None,
+            transforms.Normalize(config.train_data_mean, config.train_data_std)
+            if config.apply_normalization
+            else None,
+        ]
+        transform_test = [
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.Grayscale() if config.apply_grayscale else None,
+            transforms.ToTensor(),
+            transforms.Normalize(config.train_data_mean, config.train_data_std)
+            if config.apply_normalization
+            else None,
+        ]
+    else:
+        transform_train = [
+            transforms.RandomCrop(config.input_size, padding=4)
+            if config.apply_augmentation
+            else None,
+            transforms.RandomHorizontalFlip() if config.apply_augmentation else None,
+            transforms.RandomRotation(15) if config.apply_augmentation else None,
+            transforms.Grayscale() if config.apply_grayscale else None,
+            transforms.ToTensor(),
+            transforms.Lambda(apply_noise) if config.apply_noise else None,
+            transforms.Normalize(config.train_data_mean, config.train_data_std)
+            if config.apply_normalization
+            else None,
+        ]
+        transform_val = [
+            transforms.Grayscale() if config.apply_grayscale else None,
+            transforms.ToTensor(),
+            transforms.Lambda(apply_noise) if config.apply_noise else None,
+            transforms.Normalize(config.train_data_mean, config.train_data_std)
+            if config.apply_normalization
+            else None,
+        ]
+        transform_test = [
+            transforms.Grayscale() if config.apply_grayscale else None,
+            transforms.ToTensor(),
+            transforms.Normalize(config.train_data_mean, config.train_data_std)
+            if config.apply_normalization
+            else None,
+        ]
+    transform_test = transforms.Compose(
+        list(filter(lambda x: x is not None, transform_test))
+    )
+    transform_val = transforms.Compose(
+        list(filter(lambda x: x is not None, transform_val))
+    )
+    transform_train = transforms.Compose(
+        list(filter(lambda x: x is not None, transform_train))
+    )
 
     error_msg = "[!] valid_size should be in the range [0, 1]."
     assert (config.valid_size >= 0) and (config.valid_size <= 1), error_msg
@@ -103,14 +162,14 @@ def img_dataset_loader(seed, **config):
             root=config.data_dir,
             train=True,
             download=config.download,
-            transform=transform_base,
+            transform=transform_val,
         )
 
         test_dataset = dataset_cls(
             root=config.data_dir,
             train=False,
             download=config.download,
-            transform=transform_base,
+            transform=transform_test,
         )
     else:
         dataset_dir = get_dataset(
@@ -127,9 +186,9 @@ def img_dataset_loader(seed, **config):
 
         train_dataset = datasets.ImageFolder(train_dir, transform=transform_train)
 
-        valid_dataset = datasets.ImageFolder(train_dir, transform=transform_base)
+        valid_dataset = datasets.ImageFolder(train_dir, transform=transform_val)
 
-        test_dataset = datasets.ImageFolder(val_dir, transform=transform_base)
+        test_dataset = datasets.ImageFolder(val_dir, transform=transform_test)
 
     if config.add_corrupted_test:
         urls = DATASET_URLS[config.dataset_cls + "-C"]
@@ -158,7 +217,7 @@ def img_dataset_loader(seed, **config):
                             root=dataset_dir,
                             start=start,
                             end=end,
-                            transform=transform_base,
+                            transform=transform_test,
                         )
                 else:
                     if not os.path.isdir(os.path.join(dataset_dir, c_category)):
@@ -169,7 +228,7 @@ def img_dataset_loader(seed, **config):
                             int(c_level)
                         ] = datasets.ImageFolder(
                             os.path.join(dataset_dir, c_category, c_level),
-                            transform=transform_base,
+                            transform=transform_test,
                         )
 
     filters = [globals().get(f)(config, train_dataset) for f in config.filters]

bias_transfer/trainer/main_loop_modules/noise_augmentation.py
@@ -9,7 +9,10 @@ class NoiseAugmentation(MainLoopModule):
         super().__init__(model, config, device, data_loader, seed)
         self.rnd_gen = None
 
-    def apply_noise(self, x, device, std: dict = None, snr: dict = None, rnd_gen=None):
+    @staticmethod
+    def apply_noise(x, device, std: dict = None, snr: dict = None, rnd_gen=None, clamp=True):
+        if len(x.shape) == 3:
+            x.unsqueeze(0)  # if we only have a single element
         with torch.no_grad():
             if std:
                 noise_levels = std
@@ -52,7 +55,8 @@ class NoiseAugmentation(MainLoopModule):
                     )
                 # else: deactivate noise for a fraction of the data
                 start = end
-            x = torch.clamp(x, max=1.0, min=0.0)
+            if clamp:
+                x = torch.clamp(x, max=1.0, min=0.0)
         return x, applied_std
 
     def pre_epoch(self, model, train_mode, epoch, **kwargs):
@@ -71,5 +75,6 @@ class NoiseAugmentation(MainLoopModule):
             std=self.config.noise_std,
             snr=self.config.noise_snr,
             rnd_gen=self.rnd_gen if not train_mode else None,
+            clamp=self.config.clamp_noise
         )
         return model, inputs