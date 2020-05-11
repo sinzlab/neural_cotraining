Fixed noise augmentation after normalization

bias_transfer/trainer/main_loop_modules/noise_augmentation.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+from torchvision.transforms import transforms
 
 from .main_loop_module import MainLoopModule
 
@@ -8,11 +9,45 @@ class NoiseAugmentation(MainLoopModule):
     def __init__(self, model, config, device, data_loader, seed):
         super().__init__(model, config, device, data_loader, seed)
         self.rnd_gen = None
+        if isinstance(data_loader, dict):
+            train_loader = data_loader["img_classification"]
+        else:
+            train_loader = data_loader.loaders["img_classification"]
+        transform_list = train_loader.dataset.transforms.transform.transforms
+        # go through StandardTransform and Compose to get to  the actual transforms
+        normalization = None
+        for trans in transform_list:
+            if isinstance(trans, transforms.Normalize):
+                normalization = trans
+        if normalization:
+            # image = (image - mean) / std
+            # => noisy_image = (image + noise - mean) / std
+            #                = (image - mean) / std + noise/ std
+            img_mean = torch.tensor(normalization.mean)
+            img_std = torch.tensor(normalization.std)
+            self.img_min = -img_mean / img_std
+            self.img_max = (1 - img_mean) / img_std
+            self.noise_scale = 1 / img_std
+            self.noise_scale = self.noise_scale.view(1, -1, 1, 1).to(device)
+        else:
+            self.img_min = 0
+            self.img_max = 1
+            self.noise_scale = None
 
     @staticmethod
-    def apply_noise(x, device, std: dict = None, snr: dict = None, rnd_gen=None, clamp=True):
+    def apply_noise(
+        x,
+        device,
+        std: dict = None,
+        snr: dict = None,
+        rnd_gen=None,
+        img_min=0,
+        img_max=1,
+        noise_scale=None,
+    ):
         if len(x.shape) == 3:
             x.unsqueeze(0)  # if we only have a single element
+
         with torch.no_grad():
             if std:
                 noise_levels = std
@@ -50,13 +85,19 @@ class NoiseAugmentation(MainLoopModule):
                             std = level
                     applied_std[indices[start:end]] = std.squeeze().unsqueeze(-1)
                     std = std.expand_as(x[start:end])
-                    x[indices[start:end]] += torch.normal(
-                        mean=0.0, std=std, generator=rnd_gen
-                    )
+                    noise = torch.normal(mean=0.0, std=std, generator=rnd_gen)
+                    if noise_scale is not None:
+                        noise *= noise_scale
+                    x[indices[start:end]] += noise
                 # else: deactivate noise for a fraction of the data
                 start = end
-            if clamp:
-                x = torch.clamp(x, max=1.0, min=0.0)
+            if isinstance(img_max, torch.Tensor):
+                for i in range(
+                    img_max.shape[0]
+                ):  # clamp each color channel individually
+                    x[:, i] = torch.clamp(x[:, i], max=img_max[i], min=img_min[i])
+            else:
+                x = torch.clamp(x, max=img_max, min=img_min)
         return x, applied_std
 
     def pre_epoch(self, model, train_mode, epoch, **kwargs):
@@ -75,6 +116,8 @@ class NoiseAugmentation(MainLoopModule):
             std=self.config.noise_std,
             snr=self.config.noise_snr,
             rnd_gen=self.rnd_gen if not train_mode else None,
-            clamp=self.config.clamp_noise
+            img_min=self.img_min,
+            img_max=self.img_max,
+            noise_scale=self.noise_scale,
         )
         return model, inputs